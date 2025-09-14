5dc1b90a2cbe3676608f10cd2c256223f39def60
borysj <borysj@zohomail.eu>
2025-09-14 23:07:01 +0200

    Add remark that not subscribing emails must be hashed with SHA-256 beforehand

--- private/vip.php
+++ private/vip.php
@@ -1,6 +1,6 @@
 <?php
 // Keys are registered nicknames.
-// Values are arrays with four elements each:
+// Values are arrays with five elements each:
 // 1) Password are hashed, generate hashes like that:
 //    php -r "echo(hash('xxh3', 'ORIGINAL_PASSWORD'))";
 // 2) Nickname status: 1 for owner, 2 for eminent registered user, 3 for normal registered
@@ -11,12 +11,17 @@
 // 0 for no, 1 for yes
 // (notice that the combination of empty email and 1 for subscribtion will not
 // cause any trouble; of course, no email will be then attempted to be sent)
+// IMPORTANT! Because we want to obfuscate emails as much as possible, if the
+// last parameter is 0 (= does not want subscription) the email MUST be hashed
+// with SHA-256. The reason here is that we still need the hash for gravatar,
+// but the true email is irrelevant. You can use this command for the hash:
+// php -r "echo(hash('sha256', 'ORIGINAL_EMAIL'))";
 
 $vipNicks = [
     "JohnAuthor" => ["32bdkjkjakj25", 1, "https://mypage.com/blog", "", 0],
     "Jane Wife" => ["bkg5621ldslr35", 2, "https://example.com/stuff", "", 0],
     "Max" => ["29gjvmryd76s", 2, "", "", 0],
     "Katana8" => ["2kgj6kgur48", 3, "https://ilovejapan", "katana@ilovejapan.jp", 1],
-    "Steven Smith" => ["ghti6783skflc", 3, "", "stevensmith2819@gmail.com", 0],
+    "Steven Smith" => ["ghti6783skflc", 3, "", "55fe392f43bc9bb5da53a928cf74e6901d4eba30dd6efdaf71bfca9b2b8fabc1", 0],
 ];
 ?>
