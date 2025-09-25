ee94b4b9b2c5e1e11441f53217b90cee0a7ebf39
borysj <borysj@zohomail.eu>
2025-09-25 20:49:14 +0200

    Translate error messages to English

--- src/messages.php
+++ src/messages.php
@@ -2,21 +2,20 @@
 layout: null
 ---
 <?php
-$exitmsg_badCaptchaComment = "Czy nie jesteś przypadkiem geografem albo robotem?";
-$exitmsg_wrongPassword = "Komentarz nie został dodany.<br>Użyłeś w niewłaściwy sposób zarezerwowanego podpisu.";
-$exitmsg_errorURL = "Nastąpił błąd podczas odczytywania adresu strony,<br>gdzie ma zostać umieszczony komentarz.";
-$exitmsg_duplicate = "Wykryto duplikat komentarza.<br>Prawdopodobnie nacisnąłeś coś kilka razy pod rząd<br>albo spróbowałeś odświeżyć potwierdzenie dodania komentarza.";
-$exitmsg_success = "Komentarz dodany pomyślnie. Za 10 sekund zostaniesz przekierowany z powrotem.<br>Być może będziesz musiał wykonać pełne odświeżenie (Ctrl-F5 albo Ctrl-Shift-R), żeby zobaczyć swój komentarz.<br><br>Jeżeli chcesz zarejestrować się jako komentujący, kliknij <a href=\"{{ site.url }}/rejestracja.html\">tutaj</a>.<br><br>";
-$exitmsg_errorSavingComment = "Wystąpił błąd podczas dodawania komentarza.<br>Wróć do poprzedniej strony i spróbuj ponownie.<br>Jeżeli błąd będzie nieustępliwy, daj mi znać na $blogContactMail";
-$exitmsg_errorRunningCommentScript = "Błąd aktywacji skryptu.<br>Prawdopodobnie spróbowałeś odświeżyć stronę z potwierdzeniem dodania komentarza.<br>Jeżeli jednak błąd wyświetlił się od razu, wróć do formularza i spróbuj ponownie.<br>Jeżeli błąd będzie nieustępliwy, daj mi znać na $blogContactMail";
-$exitmsg_badCaptchaEmail = "Musisz dodać antybotowy kod do swojego adresu. Przeczytaj uważniej akapit tuż nad polem formularza.";
-$exitmsg_emailNotFound = "Nie znalazłem tego adresu emailowego na liście subskrybentów.<br>";
-$exitmsg_subscriberListNotFound = "Wskazana lista subskrybentów nie istnieje.<br>";
-$exitmsg_errorRunningSubscriberScript = "Skrypt nie został uruchomiony.<br>Czy link do odwołania subskrybcji jest poprawny?<br>Skąd żeś go wziął?";
-$exitmsg_removedSubscriber = " został usunięty z listy subskrybentów ";
-$exitmsg_cannotRemoveSubscriber = "Niezgodność haseł. Nie mogę usunąć ";
-$exitmsg_wrongCommentAdminPassword = "Nieprawidłowe hasło do edycji komentarzy jako admin.";
-$exitmsg_wrongCommentID = "BŁĄD! Nie znaleziono komentarza do edycji. Data albo kod komentarza w linku jest niepoprawny.";
-$exitmsg_tooLateToEditComment = "Już po ptokach. Na edycję tego komentarza jest za późno.";
+$exitmsg_badCaptchaComment = "I suspect you of being a robot.";
+$exitmsg_wrongPassword = "Your comment has not been added.<br>You have used a reserved nickname in a wrong way.";
+$exitmsg_errorURL = "An error occurred while reading the address of the page<br>where the comment was supposed to be added.";
+$exitmsg_duplicate = "This comment is a duplicate.<br>Probably you have clicked a button several times too quickly<br>or tried to refresh a webpage right after adding it.";
+$exitmsg_errorSavingComment = "An error occurred while saving your comment.<br>Go back and try again.<br>If the error persists, please send a message to $blogContactMail";
+$exitmsg_errorRunningCommentScript = "An error occurred while activating the comment script.<br>>Go back to the comment form and try again.<br>If the error persists, please send a message to $blogContactMail";
+$exitmsg_badCaptchaEmail = "You are supposed to add the antibot code to your email. Read carefully the instruction right above the form.";
+$exitmsg_emailNotFound = "I have not found this email on the subscriber list.";
+$exitmsg_subscriberListNotFound = "The subscriber list has not been found at all.";
+$exitmsg_errorRunningSubscriberScript = "The script has not been activated.<br>Are you sure that the link for cancelling subscription is valid?<br>Where did it come from?";
+$exitmsg_removedSubscriber = " has been removed from the subscriber list ";
+$exitmsg_cannotRemoveSubscriber = "The passwords do not match. I cannot remove ";
+$exitmsg_wrongCommentAdminPassword = "The admin password for editing comments is wrong.";
+$exitmsg_wrongCommentID = "An error has occurred. I have not found the comment to edit. The timestamp and/or the comment code in the link must be wrong.";
+$exitmsg_tooLateToEditComment = "This train has left. It is too late to edit this comment.";
 $exitmsg_wrongEmailNotificationPassword = "Are you trying to hack me?";
 $exitmsg_noSearchPhrase = "No search phrase has been provided";
