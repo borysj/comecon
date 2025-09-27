5bddb382c3cd23cd11591ab485be27ac714548e6
borysj <borysj@zohomail.eu>
2025-09-27 21:47:27 +0200

    Add missing blank argument when sending notification

--- src/email_notification.php
+++ src/email_notification.php
@@ -13,6 +13,6 @@ if (($_SERVER["REQUEST_METHOD"] === "GET") &&
     (isset($_GET["d"])) &&
     (isset($_GET["t"])) &&
     (isset($_GET["f"]))) {
-    sendNotifications($_GET["y"], $_GET["m"], $_GET["d"], $_GET["t"], "none", "", "", $_GET["f"]);
+    sendNotifications($_GET["y"], $_GET["m"], $_GET["d"], $_GET["t"], "", "none", "", "", $_GET["f"]);
 } else { exit($exitmsg_wrongEmailNotificationPassword); }
 
