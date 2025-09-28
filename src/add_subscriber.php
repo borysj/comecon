113d2f2fcd9fa996f532fd3f2c6d5230366d77ba
borysj <borysj@zohomail.eu>
2025-09-28 21:56:01 +0200

    Change comment markers

--- src/add_subscriber.php
+++ src/add_subscriber.php
@@ -12,16 +12,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["email"])) {
 
     $userEmail = prepareString($_POST["email"], 60, false, false, false);
 
-    # Check for captcha, and remove it if present.
-    # The captcha is a three characters long "secret code" at the end of the
-    # email, therefore -3.
+    // Check for captcha, and remove it if present.
+    // The captcha is a three characters long "secret code" at the end of the
+    // email, therefore -3.
     if (substr($userEmail, -3) !== $captchaEmail) { exit($exitmsg_badCaptchaEmail); }
     else { $userEmail = substr($userEmail, 0, -3); }
 
-    # If the email is not yet present in the subscribers file,
-    # add it together with a random numeric password.
-    # This password will be necessary to remove the subscriber from the file,
-    # and it will be provided in the link to cancel the subscription.
+    // If the email is not yet present in the subscribers file,
+    // add it together with a random numeric password.
+    // This password will be necessary to remove the subscriber from the file,
+    // and it will be provided in the link to cancel the subscription.
     if (filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
         if (stripos(file_get_contents($subsFilePath), $userEmail) === false) {
             $password = mt_rand(1000000,9999999);
