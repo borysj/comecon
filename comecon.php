128cdd51d5db23f146c764f0e3a94824e1396c6c
Borys Jagielski <borysj@zohomail.eu>
2025-10-15 13:21:18 +0200

    Add the post identifier at the entry point

--- comecon.php
+++ comecon.php
@@ -34,12 +34,31 @@ if (isset($_GET["action"])) {
             require __DIR__ . "/src/email_notification.php";
             break;
         case "save":
+            // We also expect $_POST["postID"], but we do not need to check for
+            // its existence. It has already been done by display_comment.php
+            // which should come before the comment submission form. If
+            // something has gone wrong there, the entire script has died
+            // already.
             validate_request("POST", ["comment", "name", "captcha", "url", "password", "webpage", "email"]);
+            $postID = $_POST["postID"];
+            $postID = preg_replace("/[^a-zA-Z0-9_\-]/", "", $postID);
+            $postID = ltrim($postID, "-");
+            $postID = substr($postID, 0, 100);
+            if ($postID === "") {
+                exit("I cannot display the comments, because the post identifier was invalid.");
+            }
             $userName = prepareString($_POST["name"], 40, false, false, false);
             $userPassword = prepareString($_POST["password"], 40, false, false, false);
             $userComment = prepareString($_POST["comment"], $settings['save']['maxCommentLength'], true, true, false);
             $userURL = prepareString($_POST["webpage"], 60, false, false, true);
             $userEmail = prepareString($_POST["email"], 60, false, false, false);
+            // $_POST["postFullTitle"] is set by the blog author in the form,
+            // and at the very least should be an empty string
+            if ($_POST["postFullTitle"] === "") {
+                $fullTitle = getFullTitle($_SERVER["REQUEST_URI"]);
+            } else {
+                $fullTitle = $_POST["postFullTitle"];
+            }
             $commenters = [];
             include "private/commenters.php";
             require __DIR__ . "/src/email_sending.php";
